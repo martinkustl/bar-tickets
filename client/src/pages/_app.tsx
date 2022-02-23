63846f0f79c170eac996a30fc6afecb1c69219d2
skepter <48717111+skepter@users.noreply.github.com>
2022-02-23 21:18:37 +0100

    move to styled components

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,18 +1,27 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import theme from '../src/theme';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
+      <GlobalStyle />
       <ThemeProvider theme={theme}>
-        <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
